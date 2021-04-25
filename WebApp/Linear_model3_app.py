c8ad52876551dc85d56e210b468a1de811c61af9
SyedKhundmir <69012420+SyedKhundmir@users.noreply.github.com>
2021-04-25 21:19:20 +0530

    version 1

---
+++ WebApp/Linear_model3_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import math
+
+pkl_filename = 'pickle_model.pkl'
+with open(pkl_filename,'rb') as file:
+    pickle_model = pickle.load(file)
+
+def load(rno1,rno2):
+    input = np.array([[rno1,rno2]]).astype(np.float)
+    prediction = math.floor(pickle_model.predict(input))
+    return prediction
+
+
+def main():
+    st.title('Predict Car Selling Price')
+    Mileage = st.text_input('Mileage','Enter Here')
+    Years = st.text_input('Years','Enter Here')
+
+    if st.button('Predict'):
+        res = load(Mileage,Years)
+        st.success('Predicted Price : {} USD'.format(res))
+
+if __name__ == '__main__':
+    main()
